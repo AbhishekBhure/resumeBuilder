bad940ff7a6c16322ea627058e69414cdae861d0
abhi <abhiab7411@gmail.com>
2023-12-17 12:28:21 +0530

    added glass effect to deleteConfirmationmodal.jsx

--- src/components/DeleteConfirmationModal.jsx
+++ src/components/DeleteConfirmationModal.jsx
@@ -4,7 +4,7 @@ const DeleteConfirmationModal = ({ isOpen, onCancel, onConfirm }) => {
   return (
     isOpen && (
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-        <div className="bg-white p-4 rounded-lg">
+        <div className="bg-tertiary bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-60 border border-gray-100 p-4 rounded-lg">
           <p>Are you sure you want to delete this resume?</p>
           <div className="flex gap-4 mt-4">
             <button
