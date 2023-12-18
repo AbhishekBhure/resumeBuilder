dee49d5b4aa9355a6185986e18ad900187b51456
abhi <abhiab7411@gmail.com>
2023-12-18 09:51:01 +0530

    modified StarRating.jsx

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -8,8 +8,6 @@ const StarRating = ({ rating }) => {
   for (let i = 1; i <= totalStars; i++) {
     if (i <= rating) {
       stars.push(<FaStar key={i} />);
-    } else if (i - 0.5 <= rating) {
-      stars.push(<FaStarHalfAlt key={i} />);
     } else {
       stars.push(<FaRegStar key={i} />);
     }
